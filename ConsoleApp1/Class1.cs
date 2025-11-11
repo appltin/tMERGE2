1d60f1487193dafb8755db38864f078aef7c4ef1
appltin <redloveandten@gmail.com>
2025-11-11 22:39:57 +0800

    pre_to_rebase

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        public int CountVowels(string input)
+        {
+            int count = 0;
+            foreach (char c in input.ToLower())
+            {
+                if ("aeiou".Contains(c))
+                    count++;
+            }
+            return count;
+        }
+    }
+}
