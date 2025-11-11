9285092a68a76a59f46d2249f954886dcc1af55f
appltin <redloveandten@gmail.com>
2025-11-11 22:49:50 +0800

    Update Class1 - 複製.cs

--- ConsoleApp1/Class1 - 複製.cs
+++ ConsoleApp1/Class1 - 複製.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp1
             int count = 45;
             foreach (char c in input.ToLower())
             {
-                if ("aeiou".Contains(c))
+                if ("aq".Contains(c))
                     count++;
             }
             return count;
